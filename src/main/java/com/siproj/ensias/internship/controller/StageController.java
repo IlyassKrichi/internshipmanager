60ae341d7c1d7b4dc02a9ee6bfbfb77baebd2ec4
Ilyass Krichi <mc.mono20@gmail.com>
2023-12-11 00:55:37 +0100

    modified:   src/main/java/com/siproj/ensias/internship/controller/StageController.java

--- src/main/java/com/siproj/ensias/internship/controller/StageController.java
+++ src/main/java/com/siproj/ensias/internship/controller/StageController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(path = "/api")
 public class StageController {
 
   private final StageService stageService;
