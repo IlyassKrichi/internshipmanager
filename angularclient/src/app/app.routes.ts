90c059ef4f742e137276e5e11c9d4e41015fe925
Ilyass Krichi <mc.mono20@gmail.com>
2023-12-19 10:00:11 +0100

    added hover effect

--- angularclient/src/app/app.routes.ts
+++ angularclient/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+];
--- angularclient/src/app/login/login.component.ts
+++ angularclient/src/app/login/login.component.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  studentsHover: boolean = false;
+  professorsHover: boolean = false;
 
+  toggleHover(group: 'students' | 'professors'): void {
+    if (group === 'students') {
+      this.studentsHover = !this.studentsHover;
+      console.log('Students Hover:', this.studentsHover);
+    } else if (group === 'professors') {
+      this.professorsHover = !this.professorsHover;
+      console.log('Professors Hover:', this.professorsHover);
+    }
+  }
 }
